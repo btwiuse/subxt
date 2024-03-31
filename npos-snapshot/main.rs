067d2f2a3b70433ef0aa4177e2a3f7c550c0d1ef
navigaid <navigaid@gmail.com>
2024-03-31 19:27:04 +0000

    "Update version in Cargo.toml to \"0.0.2\" and refactor main.rs to use SubstrateConfig instead of PolkadotConfig."

--- npos-snapshot/main.rs
+++ npos-snapshot/main.rs
@@ -1,7 +1,7 @@
 #![allow(missing_docs)]
 use clap::Parser;
 use futures::StreamExt;
-use subxt::{client::OnlineClient, lightclient::LightClient, PolkadotConfig};
+use subxt::{client::OnlineClient, lightclient::LightClient, SubstrateConfig};
 
 #[derive(clap::Parser)]
 pub struct Config {
@@ -12,31 +12,69 @@ pub struct Config {
     #[clap(short, long)]
     pub key: String,
     /// output file
-    #[clap(short, long)]
+    #[clap(short, long, default_value = "/dev/stdout")]
     pub output: String,
+}
+
+pub enum Chain {
+    Secure(String),
+    Insecure(String),
+    Light(String),
+}
+
+const POLKADOT_SPEC: &str = include_str!("polkadot.json");
+
+impl Chain {
+    pub fn parse(chain: &str) -> Self {
+        match chain {
+            x if x == "polkadot" => Chain::Light(POLKADOT_SPEC.to_string()),
+            x if x == "kusama" => Chain::Secure("wss://kusama-rpc.polkadot.io".to_string()),
+            x if x == "vara" => Chain::Secure("wss://archive-rpc.vara.network".to_string()),
+            x if x == "westend" => Chain::Secure("wss://westend-rpc.polkadot.io".to_string()),
+            x if x == "joystream" => Chain::Secure("wss://rpc.joystream.org".to_string()),
+            x if x == "enjin" => Chain::Secure("wss://rpc.relay.blockchain.enjin.io".to_string()),
+
+            url if url.starts_with("wss://") || url.starts_with("https://") => {
+                Chain::Secure(url.to_string())
+            }
+
+            url if url.starts_with("ws://") || url.starts_with("http://") => {
+                Chain::Insecure(url.to_string())
+            }
+
+            json => {
+                let chain_spec = std::fs::read_to_string(json).unwrap();
+                Chain::Light(chain_spec)
+            }
+        }
+    }
+    pub async fn api(&self) -> Result<OnlineClient<SubstrateConfig>, Box<dyn std::error::Error>> {
+        let api = match self {
+            Chain::Secure(url) => OnlineClient::<SubstrateConfig>::from_url(url).await?,
+            Chain::Insecure(url) => OnlineClient::<SubstrateConfig>::from_insecure_url(url).await?,
+            Chain::Light(spec) => {
+                let (_lightclient, rpc) = LightClient::relay_chain(spec.to_string())?;
+                OnlineClient::<SubstrateConfig>::from_rpc_client(rpc).await?
+            }
+        };
+        Ok(api)
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app = Config::parse();
 
-    // read chain spec as string
-    let chain_spec = std::fs::read_to_string(&app.chain)?;
-
-    // Instantiate a light client with the Polkadot relay chain
-    let (_lightclient, polkadot_rpc) = LightClient::relay_chain(chain_spec)?;
-
-    // Create Subxt clients from these Smoldot backed RPC clients.
-    let polkadot_api = OnlineClient::<PolkadotConfig>::from_rpc_client(polkadot_rpc).await?;
+    let api = Chain::parse(&app.chain).api().await?;
 
     // Use them!
-    let mut polkadot_sub = polkadot_api
+    let mut sub = api
         .blocks()
         .subscribe_finalized()
         .await?
-        .map(|block| (basename(&app.chain), block));
+        .map(|block| (&app.chain, block));
 
-    while let Some((chain, block)) = polkadot_sub.next().await {
+    while let Some((chain, block)) = sub.next().await {
         let block = block?;
         eprintln!(
             "Chain {:?} hash={:?} number={:?}",
@@ -44,15 +82,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             block.hash(),
             block.number()
         );
-        snapshot(&polkadot_api, &app.key, &app.output).await?;
+        snapshot(&api, &app.key, &app.output).await?;
         break;
     }
 
     Ok(())
 }
 
-async fn snapshot<T: subxt::Config>(
-    polkadot_api: &OnlineClient<T>,
+pub async fn snapshot<T: subxt::Config>(
+    api: &OnlineClient<T>,
     key: &str,
     output: &str,
 ) -> Result<(), Box<dyn std::error::Error>> {
@@ -61,27 +99,13 @@ async fn snapshot<T: subxt::Config>(
         key = key[2..].to_string();
     }
     let raw_key: Vec<u8> = hex::decode(&key)?;
-    let storage = polkadot_api
-        .storage()
-        .at_latest()
-        .await?
-        .fetch_raw(raw_key)
-        .await?;
-    if let Some(snapshot) = storage {
-        use std::io::Write;
-        let mut file = std::fs::File::create(output)?;
-        let encoded = hex::encode(&snapshot);
-        file.write_all(encoded.as_bytes())?;
-        eprintln!("{} bytes written to snapshot.hex", snapshot.len());
+    let storage = api.storage().at_latest().await?.fetch_raw(raw_key).await?;
+    if let Some(value) = storage {
+        let v = hex::encode(&value);
+        std::fs::write(&output, &v)?;
+        eprintln!("{} bytes written to {}", v.len(), output);
         Ok(())
     } else {
         Err(format!("No storage value found for key 0x{}", key).into())
     }
-}
-
-fn basename(path: &str) -> &str {
-    std::path::Path::new(path)
-        .file_name()
-        .and_then(|s| s.to_str())
-        .unwrap_or(path)
 }
