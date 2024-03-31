145586ad2aefaf7075126f5b8d14f4111ec8f1ca
navigaid <navigaid@gmail.com>
2024-03-31 18:36:48 +0800

    add snapshot example

--- subxt/examples/light_client_basic.rs
+++ subxt/examples/light_client_basic.rs
@@ -2,13 +2,7 @@
 use futures::StreamExt;
 use subxt::{client::OnlineClient, lightclient::LightClient, PolkadotConfig};
 
-// Generate an interface that we can use from the node's metadata.
-#[subxt::subxt(runtime_metadata_path = "../artifacts/polkadot_metadata_small.scale")]
-pub mod polkadot {}
-
 const POLKADOT_SPEC: &str = include_str!("../../artifacts/demo_chain_specs/polkadot.json");
-const ASSET_HUB_SPEC: &str =
-    include_str!("../../artifacts/demo_chain_specs/polkadot_asset_hub.json");
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -18,29 +12,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Instantiate a light client with the Polkadot relay chain,
     // and connect it to Asset Hub, too.
     let (lightclient, polkadot_rpc) = LightClient::relay_chain(POLKADOT_SPEC)?;
-    let asset_hub_rpc = lightclient.parachain(ASSET_HUB_SPEC)?;
 
     // Create Subxt clients from these Smoldot backed RPC clients.
     let polkadot_api = OnlineClient::<PolkadotConfig>::from_rpc_client(polkadot_rpc).await?;
-    let asset_hub_api = OnlineClient::<PolkadotConfig>::from_rpc_client(asset_hub_rpc).await?;
 
     // Use them!
-    let polkadot_sub = polkadot_api
+    let mut polkadot_sub = polkadot_api
         .blocks()
         .subscribe_finalized()
         .await?
         .map(|block| ("Polkadot", block));
-    let parachain_sub = asset_hub_api
-        .blocks()
-        .subscribe_finalized()
-        .await?
-        .map(|block| ("AssetHub", block));
 
-    let mut stream_combinator = futures::stream::select(polkadot_sub, parachain_sub);
+    let key : Vec::<u8> = hex::decode("ede8e4fdc3c8b556f0ce2f77fc2575e396d38fd45bc038faa9586fa93aa03ef7")?;
+    let storage = polkadot_api.storage().at_latest().await?.fetch_raw(key).await?;
+    if let Some(snapshot) = storage {
+        use std::io::Write;
+        let mut file = std::fs::File::create("snapshot.hex")?;
+        let encoded = hex::encode(&snapshot);
+        file.write_all(encoded.as_bytes())?;
+        println!("{} bytes written to snapshot.hex", snapshot.len());
+        return Ok(());
+    }
+    dbg!(storage);
 
-    while let Some((chain, block)) = stream_combinator.next().await {
+    while let Some((chain, block)) = polkadot_sub.next().await {
         let block = block?;
-        println!("     Chain {:?} hash={:?}", chain, block.hash());
+        println!("     Chain {:?} hash={:?} number={:?}", chain, block.hash(), block.number());
     }
 
     Ok(())
